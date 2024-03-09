c7ca6517751c76aafafbaa74e9b286e8b64c080c
Henk van Asselt <Henk.van.Asselt@gmail.com>
2024-03-09 23:04:59 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='cookiecutter-henka',
+    packages=[],
+    version='0.1.0',
+    description='Cookiecutter template for a Python package - HenkA version',
+    author='Henk van Asselt',
+    license='BSD',
+    author_email='henk.van.asselt@gmail.com',
+    url='https://github.com/HenkVanAsselt/cookiecutter-henka',
+    keywords=['cookiecutter', 'template', 'package', ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
